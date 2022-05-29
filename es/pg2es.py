7667e8e600226f1ae7e45bc29744b26bdc3ec2cb
skunz42 <skunz1@binghamton.edu>
2022-05-29 16:34:49 -0400

    changing to rest

--- es/pg2es.py
+++ es/pg2es.py
@@ -1,14 +1,18 @@
 import os
 import psycopg2
-from elasticsearch import Elasticsearch
+import requests
+#from elasticsearch import Elasticsearch #trying just the restpi
 
-es = Elasticsearch(
+'''es = Elasticsearch(
     cloud_id=os.environ['ES_CID'],
     basic_auth=(os.environ['ES_USERNAME'], os.environ['ES_PASSWORD']),
 )
 
 resp = es.info()
-print(resp)
+print(resp)'''
+
+resp = requests.get(os.environ['ES_ENDPOINT'], auth=(os.environ['ES_USERNAME'], os.environ['ES_PASSWORD']))
+print(resp.json())
 
 conn = psycopg2.connect(
     host=os.environ['PG_HOST'],

2447efc9171d519e75bf0b2c49db2a47ad0099aa
skunz42 <skunz1@binghamton.edu>
2022-05-31 12:00:31 -0400

    hopefully nohup works

--- es/pg2es.py
+++ es/pg2es.py
@@ -2,8 +2,8 @@ import os
 import psycopg2
 import requests
 
-resp = requests.get(os.environ['ES_ENDPOINT'], auth=(os.environ['ES_USERNAME'], os.environ['ES_PASSWORD']))
-print("connected to es")
+#resp = requests.get(os.environ['ES_ENDPOINT'], auth=(os.environ['ES_USERNAME'], os.environ['ES_PASSWORD']), timeout = 5)
+#print("connected to es")
 
 conn = psycopg2.connect(
     host=os.environ['PG_HOST'],
@@ -16,7 +16,7 @@ print('connected to pg')
 
 cur = conn.cursor()
 
-cur.execute("select * from posts limit 1")
+cur.execute("select * from posts")
 
 rows = cur.fetchall()
 
@@ -25,15 +25,49 @@ for row in rows:
     pid = row[0]
     about_crime = row[3]
     data = row[2]
-    print(data["title"])
+    final_data = {
+        'id': data['id'],
+        'score': data['score'],
+        'url': data['url'],
+        'name': data['name'],
+        'title': data['title'],
+        'author': data['author'],
+        'created': data['created'],
+        'permalink': data['permalink'],
+        'subreddit': data['subreddit'],
+        'created_utc': data['created_utc'],
+        'author_fullname': data['author_fullname'],
+        'subreddit_name_prefixed': data['subreddit_name_prefixed'],
+    }
     json_data = {
         '@timestamp': timestamp.isoformat(),
         'post_id': pid,
-        'data': data,
+        'data': final_data,
         'about_crime': about_crime,
     }
+    try:
+        resp = requests.post(f"{os.environ['ES_ENDPOINT']}/reddit-posts/_create/{pid}", auth=(os.environ['ES_USERNAME'], os.environ['ES_PASSWORD']), json=json_data, timeout=5)
+    except requests.exceptions.ConnectionError as e:
+        print(f"connection timed out on {pid}")
+        continue
+
+    resp_j = resp.json()
+    try:
+        print(resp_j['error']['reason'])
+    except:
+        print(f"inserting {pid}")
+        continue
+
+
 
 cur.close()
 
-'''resp = requests.post(f"{os.environ['ES_ENDPOINT']}/reddit-posts/_doc?pretty", auth=(os.environ['ES_USERNAME'], os.environ['ES_PASSWORD']), json=json_data)
-print(resp.json())'''
+
+'''json_data = {
+    "query": {
+        "match": {
+            "about_crime": False
+            }
+        }
+    }
+resp = requests.post(f"{os.environ['ES_ENDPOINT']}/reddit-posts/_delete_by_query", auth=(os.environ['ES_USERNAME'], os.environ['ES_PASSWORD']), json=json_data)'''

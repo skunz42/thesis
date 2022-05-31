69a615a3e2f9044740c5b5d37d9853a4dec9428e
skunz42 <skunz1@binghamton.edu>
2022-05-31 10:24:42 -0400

    testing one doc insert

--- es/pg2es.py
+++ es/pg2es.py
@@ -1,18 +1,9 @@
 import os
 import psycopg2
 import requests
-#from elasticsearch import Elasticsearch #trying just the restpi
-
-'''es = Elasticsearch(
-    cloud_id=os.environ['ES_CID'],
-    basic_auth=(os.environ['ES_USERNAME'], os.environ['ES_PASSWORD']),
-)
-
-resp = es.info()
-print(resp)'''
 
 resp = requests.get(os.environ['ES_ENDPOINT'], auth=(os.environ['ES_USERNAME'], os.environ['ES_PASSWORD']))
-print(resp.json())
+print("connected to es")
 
 conn = psycopg2.connect(
     host=os.environ['PG_HOST'],
@@ -22,3 +13,27 @@ conn = psycopg2.connect(
 )
 
 print('connected to pg')
+
+cur = conn.cursor()
+
+cur.execute("select * from posts limit 1")
+
+rows = cur.fetchall()
+
+for row in rows:
+    timestamp = row[1]
+    pid = row[0]
+    about_crime = row[3]
+    data = row[2]
+    print(data["title"])
+    json_data = {
+        '@timestamp': timestamp.isoformat(),
+        'post_id': pid,
+        'data': data,
+        'about_crime': about_crime,
+    }
+
+cur.close()
+
+'''resp = requests.post(f"{os.environ['ES_ENDPOINT']}/reddit-posts/_doc?pretty", auth=(os.environ['ES_USERNAME'], os.environ['ES_PASSWORD']), json=json_data)
+print(resp.json())'''

0f81e5e8e96f7d09e53a390e9ee052c63e6609d1
skunz42 <skunz1@binghamton.edu>
2022-06-01 12:51:18 -0400

    starting some basic statistics

---
+++ es/sum-hits.py
@@ -0,0 +1,34 @@
+import os
+import requests
+import csv
+
+with open('../data/urbanareas.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    next(csv_reader, None)
+
+    for row in csv_reader:
+        subreddit = row[1]
+
+        if subreddit != "seattlewa":
+            continue
+
+        json_data = {
+            'query': {
+                'term': {
+                    'data.subreddit' : subreddit,
+                },
+            },
+        }
+
+        while True:
+            try:
+                resp = requests.get(f"{os.environ['ES_ENDPOINT']}/reddit-posts/_count?pretty", auth=(os.environ['ES_USERNAME'], os.environ['ES_PASSWORD']), timeout=5, json=json_data)
+                if resp.status_code == 404:
+#                    print(f"404 error on {subreddit}")
+                    continue
+            except requests.exceptions.ConnectionError as e:
+#                print(f"connection timed out on {subreddit}")
+                continue
+
+            print(f"{resp.json()['count']}")
+            break
